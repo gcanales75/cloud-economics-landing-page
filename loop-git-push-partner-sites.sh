09333e9a5e84f2ec1c88638f063537ce7870b3a1
gilberto canales <glcnl@amazon.com>
2023-05-31 17:16:11 -0700

    first commit

---
+++ loop-git-push-partner-sites.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+gitPush = $(git add . && git commit -m "first commit" && git push -u origin main)
+$(gitPush)
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-sipser/
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-morrisopazo
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-kyndryl
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-cloudgenia
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-rackspace
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-xaldigital
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-netrix
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-escala24x7
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-totalcloud
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-sophos
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-itera
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-clouxter
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-applying
+
+# cd back to HOME
+cd /Users/glcnl/Documents/Proyectos-Latam/cloud-economics-landing-page
---
+++ set-remote-url.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-sipser/
+git remote set-url origin https://github.com/gcanales75/ce-sipser/
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-morrisopazo
+git remote set-url origin https://github.com/gcanales75/ce-morrisopazo
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-kyndryl
+git remote set-url origin https://github.com/gcanales75/ce-kyndryl
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-cloudgenia
+git remote set-url origin https://github.com/gcanales75/ce-cloudgenia
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-rackspace
+git remote set-url origin https://github.com/gcanales75/ce-rackspace
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-xaldigital
+git remote set-url origin https://github.com/gcanales75/ce-xaldigital
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-netrix
+git remote set-url origin https://github.com/gcanales75/ce-netrix
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-escala24x7
+git remote set-url origin https://github.com/gcanales75/ce-escala24x7
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-totalcloud
+git remote set-url origin https://github.com/gcanales75/ce-totalcloud
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-sophos
+git remote set-url origin https://github.com/gcanales75/ce-sophos
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-itera
+git remote set-url origin https://github.com/gcanales75/ce-itera
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-clouxter
+git remote set-url origin https://github.com/gcanales75/ce-clouxter
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/ce-applying
+git remote set-url origin https://github.com/gcanales75/ce-applying
+# cd back to HOME
+cd /Users/glcnl/Documents/Proyectos-Latam/cloud-economics-landing-page
