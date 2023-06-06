2a0f98091224fe51b76997b72f0bdfa62972f409
gilberto canales <glcnl@amazon.com>
2023-06-05 23:06:36 -0700

    first commit

---
+++ loop-git-push-partner-sites-2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/globant/
+git add . 
+git commit -m "first commit"
+git push
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/pragma/
+git add . 
+git commit -m "first commit"
+git push
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/tdsynnex/
+git add . 
+git commit -m "first commit"
+git push
+cd /Users/glcnl/Documents/Proyectos-Latam/ce-partner-sites/canvia/
+git add . 
+git commit -m "first commit"
+git push
+# cd back to HOME
+cd /Users/glcnl/Documents/Proyectos-Latam/cloud-economics-landing-page
